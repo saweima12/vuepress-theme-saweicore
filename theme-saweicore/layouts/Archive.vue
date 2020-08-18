b694ad9f16e2e6a56d1cbd08961baa1abffd1b49
Saweima <saweima12@gmail.com>
2020-08-18 22:36:00 +0800

    update example post, add archive component(on-going)

--- theme-saweicore/layouts/Archive.vue
+++ theme-saweicore/layouts/Archive.vue
@@ -1,3 +1,146 @@
 <template>
-  <div></div>
+  <main class="archive-page">
+
+    <slot name="top"/>
+
+    <div class="content-wrapper">
+      <header class="archive-header">
+        <h1 class="archive-title">{{ $page.title }}</h1>
+        <Content />
+      </header>
+
+      <section class="chapter-list">
+        <CollapsibleGroup
+          v-for="(chapter, ckey) in chapters"
+          :key="ckey"
+          :item="chapter"
+          :defaultStatus="true"
+          :eventCode="ckey == -1 ? chapter.title: null"
+          class="archive-chapter"
+        >
+          <template slot="title" v-if="ckey != -1">
+            <span class="title" >
+              {{ckey}}.{{chapter.title}}
+            </span>
+          </template>
+
+          <li
+            v-for="(section , skey) in chapter.sections.list"
+            :key="skey"
+            class="archive-section"
+          >
+            <router-link
+              :to="section.page.path"
+            >
+              <span class="section-index" v-if="ckey > -1">
+                {{ckey}}-{{section.index}}
+              </span>
+              <span class="section-name">
+                {{section.title}}
+              </span>
+            </router-link>
+          </li>
+        </CollapsibleGroup>
+      </section>
+
+      <footer class="archive-footer">
+        <div class="last-updated">
+          Last Updated:
+          <time :datetime="$page.lastUpdated">
+            {{ $page.lastUpdated }}
+          </time>
+        </div>
+      </footer>
+    </div>
+    <slot name="down"/>
+  </main>
 </template>
+
+<script>
+import CollapsibleGroup from '@theme/components/common/CollapsibleGroup';
+
+export default {
+  components: {
+    CollapsibleGroup
+  },
+  computed: {
+    chapters() {
+      return this.$page.chapters;
+    }
+  }
+
+}
+</script>
+
+<style lang="stylus">
+@import "../styles/wrapper.styl"
+
+.archive-page
+  > *
+    transition .2s ease
+    max-width $widePageWidth
+    @extend $content-wrapper, $card-box
+
+  .content-wrapper
+    box-sizing content-box
+    padding 1rem 2.5rem 2rem
+    min-height 80vh
+
+  .archive-header
+    padding 1rem .5rem 2rem
+    margin-bottom .5rem
+    border-bottom 1px solid var(--borderColor)
+
+    .archive-title
+      margin-bottom .5rem
+      font-size 28px
+      font-weight 400
+
+  .archive-footer
+    margin-top 1.5rem
+    padding 1rem .5rem
+    border-top 1px solid var(--borderColor)
+
+    .last-updated
+      display flex
+      justify-content flex-end
+
+  .collapsible-group
+    padding 0 .5rem
+    margin-bottom .5rem
+
+    .collapsible-title
+      display flex
+      align-items center
+      justify-content space-between
+      // border-bottom 1px solid var(--borderColor)
+      padding .8rem 0
+
+      .title
+        font-size 21px
+        font-weight 550
+
+    .archive-section
+      display flex
+      box-sizing border-box
+      margin-bottom .5rem
+      padding-left .8rem
+      a
+        opacity .6
+        color var(--textColor)
+        &:hover
+          color $accentColor
+
+
+      @media screen and (min-width $mq-md)
+        display inline-block
+        width calc(50% - 1rem)
+
+  @media screen and (min-width $mq-md)
+    padding 1rem .5rem 0
+
+  @media screen and (min-width $mq-lg)
+    padding 1.5rem .5rem .5rem
+
+
+</style>
